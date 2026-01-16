9e57a2cc50ef7701220e3d538e8939c2dec38605
Devesh Wadibhasme <deveshwadibhasme.12@gmail.com>
2026-01-16 23:01:58 +0530

    fix build bugs

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import FormToUpload from "./components/FormToUpload";
 import SideBar from "./components/layouts/SideBar";
-import Dashboard from "./pages/Dashboard";
 import { Outlet } from "react-router-dom";
 
 function App() {
--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -5,11 +5,10 @@ import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LogInPage from "./pages/LoginPage.jsx";
 import { AuthProvider } from "./AuthContext.jsx";
-import CarList from "./components/CarList.jsx";
 import EditForm from "./components/layouts/EditForm.jsx";
 import AddCarInfo from "./components/layouts/AddCarInfo.jsx";
 import FormToUpload from "./pages/UploadPage.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
+import Dashboard from "./pages/DashBoard.jsx";
 import VehicleList from "./pages/VehicleList.jsx";
 
 const router = createBrowserRouter([
